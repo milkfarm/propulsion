db3d42ddc75174e3f656bf2440003a1098457a1b
tim <stuff@milkfarmproductions.com>
2015-04-20 00:13:49 -0700

    Automated commit at 2015-04-20 07:13:49 UTC by middleman-deploy 1.0.0

--- assets/javascripts/tera-slider.js
+++ assets/javascripts/tera-slider.js
@@ -26,13 +26,13 @@ Made by Themanoid
         if($sldr.hasClass('multi-slides')){
             $mousePos.x = e.pageX;
             if($mousePos.x > $(window).width()/2) {
-                $sldr.css('cursor', 'url(/assets/images/tera-slider/slide-right-dark.png), e-resize');
+                $sldr.css('cursor', 'url(assets/images/tera-slider/slide-right-dark.png), e-resize');
                 if($('body').hasClass('dark-slide'))
-                    $sldr.css('cursor', 'url(/assets/images/tera-slider/slide-right.png), e-resize');
+                    $sldr.css('cursor', 'url(assets/images/tera-slider/slide-right.png), e-resize');
             } else {
-               $sldr.css('cursor', 'url(/assets/images/tera-slider/slide-left-dark.png), w-resize');
+               $sldr.css('cursor', 'url(assets/images/tera-slider/slide-left-dark.png), w-resize');
                 if($('body').hasClass('dark-slide'))
-                    $sldr.css('cursor', 'url(/assets/images/tera-slider/slide-left.png), w-resize');
+                    $sldr.css('cursor', 'url(assets/images/tera-slider/slide-left.png), w-resize');
             }
         }
     });
