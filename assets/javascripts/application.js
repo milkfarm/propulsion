dbd2361208e3d860fcbd1049fedab9ec045d4c07
tim <stuff@milkfarmproductions.com>
2015-05-19 00:10:11 -0700

    Automated commit at 2015-05-19 07:10:11 UTC by middleman-deploy 1.0.0

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -3093,33 +3093,7 @@ $(function(){
       // Disable submit button just after the form processed 1st time successfully.
       $('.msg').fadeOut();
       $('#send').attr({'disabled' : 'true', 'value' : 'Sending' });
-      /* Post Ajax function of jQuery to get all the data from the submission of the form as soon as the form sends the values to email.php*/
-      //$.post("https://getsimpleform.com/messages/ajax?form_api_token=3b8bbcbdf26210278973abe522bc23f5", $("#form").serialize(),function(result){
-        //alert(result);
-        //if(result == 'sent'){
-          ////If the email is sent successfully, remove the submit button
-          //$('#send').attr({'disabled' : 'true'});
-          //$('#send').text('Thank you!');
-        //} else {
-          //$('#send').removeAttr('disabled');
-          //$('#send').text('Error!');
-        //}
-      //});
-      // Simple Form
-      $.ajax({
-        dataType: 'jsonp',
-        url: "http://getsimpleform.com/messages/ajax?form_api_token=3b8bbcbdf26210278973abe522bc23f5",
-        data: $("#contact_form").serialize()
-      }).done(function() {
-        //callback which can be used to show a thank you message
-        //and reset the form
-        //alert("Thank you, for contacting us");
-        //window.location.href = "/confirm/";
-        clearForm('#contact_form');
-        $('#send').attr({'disabled' : 'true'});
-        $('#send').text('Thank you!');
-        $('.notice').fadeIn();
-      });
+      $('#contact_form').submit();
     } else {
       $('.msg').fadeIn();
     }
